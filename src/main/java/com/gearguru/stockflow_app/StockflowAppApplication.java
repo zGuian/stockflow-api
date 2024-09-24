21177bb71c347237d9e5adb90cbf2558a0891799
Guian Rocha <guianmarcos32@gmail.com>
2024-09-24 16:27:56 -0300

    Add archives

---
+++ src/main/java/com/gearguru/stockflow_app/StockflowAppApplication.java
@@ -0,0 +1,13 @@
+package com.gearguru.stockflow_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockflowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockflowAppApplication.class, args);
+	}
+
+}
